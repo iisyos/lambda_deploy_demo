d66c4f4243dd0b84efbff7f4c2051f3f7a643d0b
iisyos <kosuke1216a@yahoo.co.jp>
2022-10-16 00:58:34 +0900

    wip

--- src/app.py
+++ src/app.py
@@ -26,7 +26,7 @@ from sympy import content
 import turicreate as tc
 
 import boto3
-
+import joblib
 
 logger = logging.getLogger(__name__)
 
@@ -94,7 +94,6 @@ async def client_exception_handler(request: Request, exc: ImageNotDownloadedExce
 @app.on_event('startup')
 def load_model():
     print('start')
-    # Load images (Note:'Not a JPEG file' errors are warnings, meaning those files will be skipped)
 
 def configure_logging(logging_level=logging.INFO):
     """
@@ -108,65 +107,6 @@ def configure_logging(logging_level=logging.INFO):
     root.setLevel(logging_level)
     root.addHandler(stream_handler)
 
-# def get_url_scheme(url, default_scheme='unknown'):
-#     """
-#     Returns the scheme of the specified URL or 'unknown' if it could not be determined.
-#     """
-#     result = urlparse(url, scheme=default_scheme)
-#     return result.scheme
-
-
-# async def retrieve_content(url, sess):
-#     """
-#     Retrieves the image content for the specified entry.
-#     """
-#     logger.info('retrieve_content.')
-#     if url is not None:
-#         scheme = get_url_scheme(url)
-#         if scheme in ('http', 'https'):
-#             raw_data = await download(url, sess)
-#         else:
-#             raise ValueError('Invalid scheme: %s' % scheme)
-#         if raw_data is not None:
-#             image = Image.open(io.BytesIO(raw_data))
-#             image = image.convert('RGB')
-#             return image
-#     return None
-
-# async def retrieve_image(url):
-#     """
-#     Retrieves the images for processing.
-
-#     :param entries: The entries to process.
-
-#     :return: The retrieved data.
-#     """
-#     logger.info('ClientSession.')
-#     async with ClientSession() as sess:
-#         print('ClientSession')
-            
-#         return await retrieve_content(url, sess),
-            
-
-# async def download(url, sess):
-#     """
-#     Downloads an image from the specified URL.
-
-#     :param url: The URL to download the image from.
-#     :param sess: The session to use to retrieve the data.
-#     :param sem: Used to limit concurrency.
-
-#     :return: The file's data.
-#     """
-#     async with sess.get(url) as res:
-#         logger.info('Downloading %s' % url)
-#         content = await res.read()
-#         logger.info('Finished downloading %s' % url)
-#     if res.status != 200:
-#         raise ImageNotDownloadedException('Could not download image.')
-#     return content
-
-
 def predict_images(url):
     """
     Predicts the image's category and transforms the results into the output format.
@@ -175,10 +115,9 @@ def predict_images(url):
 
     :return: The prediction results.
     """
-    loaded_model = tc.load_model('./cats-dogs.model')
+    loaded_model = tc.load_model('cats-dogs.model')
     data = tc.image_analysis.load_images(url)
     return loaded_model.predict(data)[0]
-
 
 
 @app.post('/v1/predict', response_model=PredictResponse)
@@ -214,9 +153,9 @@ def test():
     """
 
     list = [Envs(key=e,val=os.getenv(e)) for e in os.environ ]
+
+
     return HealthCheck(contents=list)
-
-
 
 handler = Mangum(app)
 
